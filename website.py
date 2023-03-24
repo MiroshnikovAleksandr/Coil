918521bde37a1c8e77a17425c14593baaf72f18a
MiroshnikovAleksandr <alexmoff04@yandex.ru>
2023-03-24 13:45:05 +0300

    website.py added with streamlit

---
+++ website.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import tomli
+import DEAP_Field_refactored as alg
+
+st.title('Project')
+
+with open('parameters.toml', 'rb') as toml:
+    parameters = tomli.load(toml)
+
+minimal_gap = st.number_input('Diameter of your wire, m', value=0.001)
+parameters['geom']['minimal_gap'] = minimal_gap
+
+geometry = st.selectbox('Select the geometry of your coil', ('Circular', 'Square', 'Piecewise linear'))
+
+if geometry == 'Circular':
+    parameters['geom']['a_max'] = st.number_input('Maximum radius of your coil, m', value=0.5)
+    parameters['geom']['a_min'] = st.number_input('Minimal radius of your coil, m', value=0.05)
+
+GA = alg.Genetic(parameters)
+GA.alg.preparation()
+GA.alg.execution()
+GA.alg.show()
+
