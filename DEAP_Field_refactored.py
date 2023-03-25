3d905332fcac8e4de4ab572da68580e51859ec37
MiroshnikovAleksandr <alexmoff04@yandex.ru>
2023-03-25 03:38:43 +0300

    professional comments

--- DEAP_Field_refactored.py
+++ DEAP_Field_refactored.py
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import Field_functions as ff
-import tomllib
+import tomli
 
 with open('parameters.toml', 'rb') as toml:
-    parameters = tomllib.load(toml)
+    parameters = tomli.load(toml)
 
 toolbox = base.Toolbox()  # create toolbox for genetic algorithm
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
@@ -39,6 +39,13 @@ class Genetic:
         self.minimal_gap = params['geom']['minimal_gap']
 
     def preparation(self):
+        """
+        Describes and generates basic genetic algorithm objects:
+        an individual is a list of random length consisting of 1s and 0s, that encodes
+        a sequence of turn radiuses of a spiral coil;
+        the population is a set of individuals.
+        @return:
+        """
         toolbox.register("ZeroOrOne", random.randint, 0, 1)
         toolbox.register("individual", tools.initRepeat, creator.Individual,
                          toolbox.ZeroOrOne, random.randint(50, 1000))
@@ -46,7 +53,13 @@ class Genetic:
 
         self.pop = toolbox.population(n=self.population_size)
 
-    def bounds_fn(self, ind: creator.Individual):
+    def bounds_fn(self, ind):
+        """
+        Calculates boundaries of coil turn placement based on the diameter of the wire;
+        sorts the turns of an individual (coil) in descending order.
+        @param ind: creator.Individual
+        @return: list
+        """
         ind = [0] * (self.len_of_turn - (len(ind) % self.len_of_turn)) + ind
 
         bounds = [(self.a_min, self.a_max)]
@@ -77,7 +90,12 @@ class Genetic:
 
         return [sorted_individual, bounds]
 
-    def decode_all_x(self, individual: creator.Individual):
+    def decode_all_x(self, individual):
+        """
+        Decodes the individual from 1-0 code to a sequence of radiuses of turns of the coil.
+        @param individual: creator.Individual
+        @return: list
+        """
         len_chromosome_one_var = self.len_of_turn
         bound_index = 0
         x = []
@@ -101,8 +119,13 @@ class Genetic:
 
         return x
 
-    def objective_fxn(self, individual: creator.Individual):
-        # decoding chromosome to get decoded x in a list
+    def objective_fxn(self, individual):
+        """
+        This is the objective function of the genetic algorithm. It returns the coefficient of variation
+        of the magnetic field induced by the coil.
+        @param individual: creator.Individual
+        @return: list
+        """
         r_i = self.decode_all_x(individual)
         Bz = ff.Bz(self.a_max, self.a_min, len(r_i), self.I, self.spacing, self.cp, r_i)
 
@@ -112,7 +135,13 @@ class Genetic:
         obj_function_value = COV
         return [obj_function_value]
 
-    def mutate(self, ind: creator.Individual, Indpb: float):
+    def mutate(self, ind, Indpb):
+        """
+        The mutation algorithm.
+        @param ind: creator.Individual
+        @param Indpb: float
+        @return: creator.Individual
+        """
         p = random.random()
         if p <= 0.5:
             ind += [(i - (i - 1)) * random.randint(0, 1) for i in range(self.len_of_turn)]
@@ -121,17 +150,32 @@ class Genetic:
         ind = tools.mutFlipBit(ind, indpb=Indpb)
         return ind
 
-    def check_feasibility(self, ind: creator.Individual):
+    def length(self, ind):
+        """
+        Calculates the length  of the coil.
+        @param ind: creator.Individual
+        @return: float
+        """
+        l = 2 * math.pi * np.sum(np.array(self.decode_all_x(ind)))
+        return l
+
+    def check_feasibility(self, ind):
+        """
+        Checks the feasibility of an individual: whether the length of the coil is adequate.
+        @param ind: creator.Individual
+        @return: boolean
+        """
         if self.length(ind) > 100:
             return False
         else:
             return True
 
-    def length(self, ind: creator.Individual):
-        l = 2 * math.pi * np.sum(np.array(self.decode_all_x(ind)))
-        return l
-
     def execution(self):
+        """
+        Executes the genetic algorithm with selection, mating and mutation.
+        Also stores the best individual from the perspective of its objective function in @hall_of_fame@.
+        @return: list
+        """
         # registering objective function with constraint
         toolbox.register("evaluate", self.objective_fxn)  # privide the objective function here
         toolbox.decorate("evaluate",
@@ -163,6 +207,10 @@ class Genetic:
         return self.objective_fxn(self.hall_of_fame[0])
 
     def show(self):
+        """
+        Displays the results (statistics plot, best COV, total length of the best individual).
+        @return:
+        """
         # using select method in logbook object to extract the argument/key as list
         plt.plot(self.logbook.select('Min'))
 
@@ -176,7 +224,7 @@ class Genetic:
         print(self.objective_fxn(self.hall_of_fame[0])[0])
 
         print(f'Total length = {self.length(self.hall_of_fame[0])} m.')
-        plt.show()
+        #plt.show()
 
         # df = pd.DataFrame(self.decode_all_x(hall_of_fame[0]))
         # df.to_excel('hall_of_fame.xlsx')
